441a33878f5123ddac93308ee9f155f1caf814cf
root <root@MBD-CKANCEL.>
2025-03-04 11:02:03 +0100

    sql app

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+st.write("Hello World!")
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+
+tab1, tab2, tab3 = st.tabs(["S", "Q", "L"])
+
+with tab1:
+    st.dataframe(df)
+    query = st.text_area(label="entrez votre query")
+    result = duckdb.sql(query).df()
+    st.write(f"Vous avez entré la query suivant: {query}")
+    st.dataframe(result)
+
+with tab2:
+    st.header("oui")
+
+with tab3:
+    st.header("non")
